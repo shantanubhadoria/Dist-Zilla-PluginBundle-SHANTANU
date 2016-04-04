21eb31161de80edc5a91dbdb21bb443adf4b4a60
Shantanu Bhadoria <shantanu@cpan.org>
2016-04-04 15:08:35 +0800

    Build results of b5f9f00 (on master)

--- lib/Dist/Zilla/PluginBundle/SHANTANU.pm
+++ lib/Dist/Zilla/PluginBundle/SHANTANU.pm
@@ -557,6 +557,20 @@ __END__
 
 Dist::Zilla::PluginBundle::SHANTANU - Dist Zilla Plugin Bundle the way I like to use it
 
+
+
+=begin html
+
+<p>
+<img src="https://img.shields.io/badge/perl-unknown-brightgreen.svg" alt="Requires Perl unknown" />
+<a href="https://travis-ci.org/shantanubhadoria/perl-Math-KalmanFilter"><img src="https://api.travis-ci.org/shantanubhadoria/perl-Math-KalmanFilter.svg?branch=build/master" alt="Travis status" /></a>
+<a href="http://matrix.cpantesters.org/?dist=Dist-Zilla-PluginBundle-SHANTANU%200.39"><img src="https://badgedepot.code301.com/badge/cpantesters/Dist-Zilla-PluginBundle-SHANTANU/0.39" alt="CPAN Testers result" /></a>
+<a href="http://cpants.cpanauthors.org/dist/Dist-Zilla-PluginBundle-SHANTANU-0.39"><img src="https://badgedepot.code301.com/badge/kwalitee/Dist-Zilla-PluginBundle-SHANTANU/0.39" alt="Distribution kwalitee" /></a>
+<a href="https://gratipay.com/shantanubhadoria"><img src="https://img.shields.io/gratipay/shantanubhadoria.svg" alt="Gratipay" /></a>
+</p>
+
+=end html
+
 =head1 VERSION
 
 version 0.39
